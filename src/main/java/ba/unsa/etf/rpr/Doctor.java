6d976ac6bc139ff7cc8816c45fe1a1787158c870
aawad1 <aawad1@etf.unsa.ba>
2023-08-10 14:49:46 +0200

    klasa doctor napravljena

---
+++ src/main/java/ba/unsa/etf/rpr/Doctor.java
@@ -0,0 +1,66 @@
+package ba.unsa.etf.rpr;
+
+import java.util.Objects;
+
+public class Doctor implements Idable, Passwordabel{
+        private int id;
+        private String Name;
+        private String password;
+
+        // Constructors
+
+        public Doctor(int id, String name) {
+            this.id = id;
+            Name = name;
+            this.password = password;
+        }
+
+
+    //Getters and Setters
+
+        public int getId() {
+            return id;
+        }
+
+        public String getName() {
+            return Name;
+        }
+
+        public String getPassword() {
+            return password;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public void setName(String name) {
+            Name = name;
+        }
+
+        public void setPassword(String password) {
+            this.password = password;
+        }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Doctor doctor = (Doctor) o;
+        return id == doctor.id && Objects.equals(Name, doctor.Name) && Objects.equals(password, doctor.password);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, Name, password);
+    }
+
+    @Override
+    public String toString() {
+        return "Doctor{" +
+                "Name='" + Name +
+                '}';
+    }
+}
+
+
--- src/main/java/ba/unsa/etf/rpr/Patient.java
+++ src/main/java/ba/unsa/etf/rpr/Patient.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.rpr;
 
 import java.util.Date;
+import java.util.Objects;
 
 public class Patient implements Idable, Passwordabel{
     private int id;
@@ -8,9 +9,32 @@ public class Patient implements Idable, Passwordabel{
     private Date dateOfBirth;
     private String phoneNumber;
     private String email;
-    private String address;
 
     // Constructors
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Patient patient = (Patient) o;
+        return id == patient.id && Objects.equals(Name, patient.Name) && Objects.equals(dateOfBirth, patient.dateOfBirth) && Objects.equals(phoneNumber, patient.phoneNumber) && Objects.equals(email, patient.email);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, Name, dateOfBirth, phoneNumber, email);
+    }
+
+    @Override
+    public String toString() {
+        return "Patient{" +
+                "id=" + id +
+                ", Name='" + Name + '\'' +
+                ", dateOfBirth=" + dateOfBirth +
+                ", phoneNumber='" + phoneNumber + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
 
     public Patient(int id, String Name, Date dateOfBirth, String phoneNumber, String email, String address) {
         this.id = id;
@@ -18,7 +42,6 @@ public class Patient implements Idable, Passwordabel{
         this.dateOfBirth = dateOfBirth;
         this.phoneNumber = phoneNumber;
         this.email = email;
-        this.address = address;
     }
 
     public Patient() {}
@@ -64,12 +87,5 @@ public class Patient implements Idable, Passwordabel{
         this.email = email;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
 }
 
