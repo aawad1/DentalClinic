29ff2e8022d45342c1908a0b695ab77772ee0a75
aawad1 <aawad1@etf.unsa.ba>
2023-08-10 20:07:01 +0200

    PacijentDao kreirana

--- src/main/java/ba/unsa/etf/rpr/dao/PatientDao.java
+++ src/main/java/ba/unsa/etf/rpr/dao/PatientDao.java
@@ -1,4 +1,27 @@
 package ba.unsa.etf.rpr.dao;
 
-public interface PatientDao {
-}
+import ba.unsa.etf.rpr.domain.Doctor;
+import ba.unsa.etf.rpr.domain.Patient;
+
+import java.util.List;
+
+    public interface PatientDao extends Dao<Patient>{
+        /**
+         * Method that lists out the patients that belong to the same doctor
+         * @param doctor doctor whose patients you are searching
+         * @return list of patients
+         */
+        List<Patient> searchByDoctor(Doctor doctor);
+
+        /**
+         * Method that lists out the patients that have the specified
+         * name and password
+         * @param name of the patient
+         * @param pass of the patient
+         * @return list of patients that have the same name and password
+         */
+
+        List<Patient> searchByNameAndUin(String name, Long UIN);
+
+    }
+
